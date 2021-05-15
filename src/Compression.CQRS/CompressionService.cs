cacaf8752229208037b5f2baf802874e7002b7c7
Julia Tolipova <yulia.tolipova@isobar.com>
2021-05-15 19:28:41 +0200

    init thresing compression

--- src/Compression.CQRS/CompressionService.cs
+++ src/Compression.CQRS/CompressionService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.IO.Compression;
+using System.Threading;
 
 namespace Compression.CQRS
 {
@@ -8,7 +9,18 @@ namespace Compression.CQRS
     {
         private const string _compressionExtension = ".gz";
 
-        private const int _blockSize = 1222;
+        // chunks of 1Mb size
+        private const int _chunkSize = 1024 * 1024;
+
+        private readonly object _lock = new object();
+
+        private static TaskExecutor _threadPool;
+        private static int _concurrentEntriesSize = Environment.ProcessorCount * 4;
+
+        public CompressionService()
+        {
+            _threadPool = new TaskExecutor();
+        }
 
         public void Compress(string fileNameToCompress, string archiveFileName)
         {
@@ -26,9 +38,14 @@ namespace Compression.CQRS
                 throw new UnauthorizedAccessException($"File {fileToCompress.FullName} is not accesible");
             }
 
-            if (fileToCompress.Extension != _compressionExtension)
+            if (fileToCompress.Extension == _compressionExtension)
             {
                 throw new FileNotFoundException($"File {fileToCompress.FullName} is already compressed");
+            }
+
+            if (fileToCompress.Length == 0)
+            {
+                throw new FileLoadException($"File {fileToCompress.FullName} is empty");
             }
 
             // delete the file if it's already exists
@@ -41,22 +58,75 @@ namespace Compression.CQRS
             {
                 using (FileStream compressedFileStream = File.Create(archiveFile.FullName + _compressionExtension))
                 {
-                    using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
-                    {
-                        originalFileStream.CopyTo(compressionStream);
-                    }
+                    CompressInChunks(originalFileStream, compressedFileStream);
                 }
-                FileInfo info = new FileInfo(archiveFile.FullName + _compressionExtension);
-                Console.WriteLine($"Compressed {fileToCompress.Name} from {fileToCompress.Length} to {info.Length} bytes.");
-
             }
-
-            //File.Move(fileToCompress.FullName + _compressionExtension, archiveFile.FullName + _compressionExtension);
         }
 
         public void Decompress(string archiveFileName, string decompressedFileName)
         {
             throw new NotImplementedException();
+        }
+
+        private void CompressInChunks(Stream source, Stream target)
+        {
+            var blockAmount = (int)(source.Length / _chunkSize + (source.Length % _chunkSize > 0 ? 1 : 0));
+
+            var blockCurrent = 0;
+
+            target.Seek(0, SeekOrigin.Begin);
+            source.Seek(0, SeekOrigin.Begin);
+
+            var resetEvent = new AutoResetEvent(false);
+
+            while (source.Position < source.Length)
+            {
+                var data = new byte[_chunkSize];
+                var dataLength = source.Read(data, 0, _chunkSize);
+                
+                AddToQueue(() =>
+                {
+                    lock (_lock)
+                    {
+
+                        using (var result = new MemoryStream())
+                        {
+                            using (var compressionStream = new GZipStream(result, CompressionMode.Compress))
+                            {
+                                compressionStream.Write(data, 0, dataLength);
+                            }
+                            target.Write(result.ToArray(), 0, result.ToArray().Length);
+                        }
+
+                        // release if the last block was processed
+                        if (++blockCurrent == blockAmount)
+                            resetEvent.Set();
+
+                    }
+                });
+            }
+            
+            resetEvent.WaitOne();
+        }
+
+        private void AddToQueue(Action action)
+        {
+            _threadPool.AddTask(() => {
+                try
+                {
+                    action();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            });
+            _threadPool.Start();
+        }
+
+        public void Dispose()
+        {
+            _threadPool.Dispose();
         }
     }
 }
---
+++ src/Compression.CQRS/TaskExecutor.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Compression.CQRS
+{
+    public sealed class TaskExecutor : IDisposable
+    {
+        private readonly IList<Thread> _threadPool;
+
+        private readonly Queue<Action> _actions = new Queue<Action>();
+
+        private volatile bool _isDisposed;
+
+        private volatile bool _isExecuting;
+        private readonly int _maxThreadsCount = Environment.ProcessorCount;
+
+        private readonly object _lock = new object();
+        private int _totalTasks;
+
+        private event EventHandler<Thread> TaskDone;
+
+        public TaskExecutor()
+        {
+            _threadPool = new List<Thread>(_maxThreadsCount);
+        }
+
+        ~TaskExecutor()
+        {
+            if (!_isDisposed)
+            {
+                Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            _isExecuting = false;
+
+            _isDisposed = true;
+
+            _actions.Clear();
+
+            foreach (var _thread in _threadPool)
+                _thread.Join();
+        }
+
+        public void AddTask(Action task)
+        {
+            _actions.Enqueue(task);
+        }
+
+        public void Start()
+        {
+            _isExecuting = true;
+
+            while (true)
+            {
+                if (!_isExecuting || (_actions.Count == 0 && _threadPool.Count == 0)) // Stop execution or all tasks done.
+                    break;
+
+                if (_threadPool.Count == _maxThreadsCount) // The whole pool is busy with threads.
+                    continue;
+
+                Action task;
+
+                if (!_actions.TryDequeue(out task)) // All tasks from the queue are pulled out.
+                    continue;
+
+                TaskDone = (sender, args) => { lock (_lock) _threadPool.Remove(args); };
+                _totalTasks++;
+
+                var thread = new Thread(() => { task(); TaskDone?.Invoke(this, Thread.CurrentThread); }) { Name = "GZipTask" + _totalTasks, Priority = ThreadPriority.AboveNormal };
+                lock (_lock) _threadPool.Add(thread);
+                thread.Start();
+            }
+
+            _isExecuting = false;
+            _totalTasks = 0;
+        }
+    }
+}
